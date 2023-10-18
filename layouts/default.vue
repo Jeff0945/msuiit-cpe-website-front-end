2015be48e22f87a54051ed8ca34e07ae6944683e
Jeff Froilan Fortunado <jeff0945@gmail.com>
2023-10-18 22:12:03 +0800

    Added header

---
+++ layouts/default.vue
@@ -0,0 +1,70 @@
+<template>
+  <header class="bg-white">
+    <Title>{{ title }}</Title>
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="flex lg:flex-1">
+        <a href="#" class="-m-1.5 p-1.5">
+          <span class="sr-only">{{ title }}</span>
+          <img class="h-11 w-auto" src="/img/icpepse-msuiit-logo.jpg" alt="" />
+        </a>
+      </div>
+      <div class="flex lg:hidden">
+        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+      <PopoverGroup class="hidden lg:flex lg:gap-x-12">
+        <NuxtLink to="/" class="text-sm font-semibold leading-6 text-gray-900">Home</NuxtLink>
+        <NuxtLink to="/merch" class="text-sm font-semibold leading-6 text-gray-900">Merch</NuxtLink>
+        <NuxtLink to="/company" class="text-sm font-semibold leading-6 text-gray-900">About Us</NuxtLink>
+      </PopoverGroup>
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+      </div>
+    </nav>
+    <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+      <div class="fixed inset-0 z-10" />
+      <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div class="flex items-center justify-between">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">{{ title }}</span>
+            <img class="h-11 w-auto" src="/img/icpepse-msuiit-logo.jpg" alt="" />
+          </a>
+          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+            <span class="sr-only">Close menu</span>
+            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <NuxtLink to="/" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Home</NuxtLink>
+              <NuxtLink to="/merch" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Merch</NuxtLink>
+              <NuxtLink to="/company" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About Us</NuxtLink>
+            </div>
+            <div class="py-6">
+              <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
+            </div>
+          </div>
+        </div>
+      </DialogPanel>
+    </Dialog>
+  </header>
+  <slot/>
+</template>
+
+<style>
+.router-link-active {
+  color: #24356e;
+}
+</style>
+
+<script setup>
+import { ref } from 'vue'
+import { DialogPanel, PopoverGroup } from "@headlessui/vue";
+
+const mobileMenuOpen = ref(false)
+
+const title = useState('title', () => 'ICpEP.SE MSU-IIT')
+</script>
