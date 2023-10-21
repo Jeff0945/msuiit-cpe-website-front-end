8d19ca6d3855ee8cc64f11368ceeaa6c0b483175
Jeff Froilan Fortunado <jeff0945@gmail.com>
2023-10-21 11:57:57 +0800

    Coming soon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      link: [{
+        rel: 'icon',
+        type: 'image/jpeg',
+        href: '/img/icpepse-msuiit-logo.jpg'
+      }]
+    },
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
